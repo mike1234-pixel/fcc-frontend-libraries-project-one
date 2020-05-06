979799892a8540c87249308ed9773e4cc0e39607
Michael Tandy <mike.tandy.1@gmail.com>
2020-05-06 21:05:39 +0100

    Updates

---
+++ precache-manifest.9ed758f161717daf51f2983d1d50c25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fca0693e29584bbf093dc6067a20fc6",
+    "url": "/LOTRQuote/index.html"
+  },
+  {
+    "revision": "e454e41cfef68f195141",
+    "url": "/LOTRQuote/static/css/main.17d7ec02.chunk.css"
+  },
+  {
+    "revision": "c8a3d179ecf50ee60a7f",
+    "url": "/LOTRQuote/static/js/2.f5dd36f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LOTRQuote/static/js/2.f5dd36f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e454e41cfef68f195141",
+    "url": "/LOTRQuote/static/js/main.96a173a6.chunk.js"
+  },
+  {
+    "revision": "ab1d626c6c7d5b33a2d2",
+    "url": "/LOTRQuote/static/js/runtime-main.dc4283df.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/LOTRQuote/precache-manifest.9ed758f161717daf51f2983d1d50c25e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LOTRQuote/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
